b01855e7ec100ee997b5143676fb5f7f34081035
Erick Ricardo <edck030303@gmail.com>
2025-02-01 17:49:14 -0300

    commit inicial projeto loja virtual ricowl

---
+++ src/main/java/com/example/demo/LojaVirtualRicowlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaVirtualRicowlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaVirtualRicowlApplication.class, args);
+	}
+
+}
