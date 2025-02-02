37108e7296c172162c9ff0803319a2ef8adceea8
Erick Ricardo <edck030303@gmail.com>
2025-02-02 19:36:16 -0300

    commit classe categoria produto

--- src/main/java/lojavirtual/ricowl/model/CategoriaProduto.java
+++ src/main/java/lojavirtual/ricowl/model/CategoriaProduto.java
@@ -11,18 +11,19 @@ import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
+
 @Entity
-@Table(name = "marca_produto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
-public class MarcaProduto implements Serializable {
+@Table(name = "categoria_produto")
+@SequenceGenerator(name = "seq_categoria_produto", sequenceName = "seq_categoria_produto", allocationSize = 1, initialValue = 1)
+public class CategoriaProduto implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_categoria_produto")
 	private Long id;
 	
-	@Column(nullable = false)
+	@Column(name = "nome_descricao", nullable = false)
 	private String nomeDesc;
 
 	public Long getId() {
@@ -57,7 +58,7 @@ public class MarcaProduto implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		MarcaProduto other = (MarcaProduto) obj;
+		CategoriaProduto other = (CategoriaProduto) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -66,6 +67,7 @@ public class MarcaProduto implements Serializable {
 		return true;
 	}
 
-	
 
+	
+	
 }
